0072ce42974e180260e961fbe7748a869143e3bc
dvellucci <vellucci.dan@gmail.com>
2018-04-26 16:37:14 -0400

    VBO creation
    Created the vertex buffer object.

--- OpenGL_Base/window.cpp
+++ OpenGL_Base/window.cpp
@@ -8,6 +8,19 @@ window::window(int width, int height) : m_screenWidth(width), m_screenHeight(hei
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f,  0.5f, 0.0f
+	};
+
+	//create a VBO with a size of 1
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	//bind the created buffer with GL_ARRAY_BUFFER
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); //STATIC draw is used since the image will not be changing often
+
 	//create the main window and if it is successful, make the context
 	mainWindow = glfwCreateWindow(m_screenWidth, m_screenWidth, "LearnOpenGL", NULL, NULL);
 	if (mainWindow == NULL)
